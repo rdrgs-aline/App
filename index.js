c05224350aca7990af616f55d94cc11713e95934
aar.alinerodrigues@gmail.com <aar.alinerodrigues@gmail.com>
2024-09-12 14:23:44 -0300

    adiciona metas abertas

--- index.js
+++ index.js
@@ -61,10 +61,25 @@ const metasRealizadas = async () => {
         return
     }
     await select({
-        message: "Meta Realizadas",
+        message: "Meta Realizadas" + realizadas.length,
         choices: [...realizadas]
     })
 
+}
+
+const metasAbertas = async () => {
+    const abertas = metas.filter((meta) => {
+        return meta.checked != true
+    })
+
+    if(abertas.length == 0){
+        console.log("Não existem metas abertas! :)")
+        return
+    }
+    await select({
+        message: "Metas Abertas" + abertas.length,
+        choices: [...abertas]
+    })
 }
 
 const start = async () => {
@@ -87,6 +102,10 @@ const start = async () => {
                 value: "realizadas"
             },
             {
+                name: "Metas abertas",
+                value: "abertas"
+            },
+            {
                 name: "Sair",
                 value: "sair"
                 }
@@ -105,6 +124,9 @@ const start = async () => {
                 break;
             case "realizadas":
                 await metasRealizadas()
+                break;
+            case "abertas":
+                await metasAbertas()
                 break;
             case "sair":
                     console.log("Até a próxima!")
